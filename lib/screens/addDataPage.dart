852e19e7087bf4824ec01eb8ac861071c4ce11cd
Haresh Nayak <61956975+hareshnayak@users.noreply.github.com>
2020-08-29 15:46:31 +0530

    updated UI to add lecture &announcement

--- lib/screens/addDataPage.dart
+++ lib/screens/addDataPage.dart
@@ -48,3 +48,188 @@ class _AddDataPageState extends State<AddDataPage> {
   }
 
 }
+
+
+
+// ListView(
+//         children: <Widget>[
+// //           Center(
+// //             child: 
+//             Container(
+//               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+//               child: Text('Add Notification',
+//                   style: TextStyle(
+//                       color: Colors.black,
+//                       fontSize: 30,
+//                       fontWeight: FontWeight.bold)),
+// //             ),
+//           ),
+//           SizedBox(height: 20),
+//           Container(
+//             margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+//             padding: EdgeInsets.all(0),
+//             height: 70,
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.all(
+//                 Radius.circular(10),
+//               ),
+//               color: Colors.white,
+//               boxShadow: [
+//                 BoxShadow(
+//                   offset: Offset(0.0, 0.4),
+//                   blurRadius: 3,
+//                   color: Colors.grey.shade300,
+//                 )
+//               ],
+//               border: Border.all(color: Colors.grey.shade300),
+//             ),
+//             child: FlatButton(
+//               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+//               onPressed: () {
+// //                   showDialog(
+// //                     context: context,
+// //                     builder: (_) => AddLecture(),
+// //                   );
+//               },
+//               child: Column(children: <Widget>[
+//                 Row(
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     children: <Widget>[
+//                       Padding(
+//                         padding: EdgeInsets.symmetric(horizontal: 5),
+//                         child: Icon(Icons.edit, color: Colors.green, size: 30),
+//                       ),
+//                       Container(
+//                         margin: EdgeInsets.only(left: 2),
+//                         height: 25,
+//                         width: 2,
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.all(
+//                             Radius.circular(10),
+//                           ),
+//                           color: Colors.green,
+//                         ),
+//                       ),
+//                       Container(
+//                         margin: EdgeInsets.symmetric(horizontal: 2),
+//                         height: 45,
+//                         width: 2,
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.all(
+//                             Radius.circular(10),
+//                           ),
+//                           color: Colors.green,
+//                         ),
+//                       ),
+//                       Container(
+//                         margin: EdgeInsets.only(right: 10),
+//                         height: 25,
+//                         width: 2,
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.all(
+//                             Radius.circular(10),
+//                           ),
+//                           color: Colors.green,
+//                         ),
+//                       ),
+//                       Container(
+//                           child: Column(
+//                               mainAxisAlignment: MainAxisAlignment.start,
+//                               crossAxisAlignment: CrossAxisAlignment.start,
+//                               children: <Widget>[
+//                             Text('Lecture',
+//                                 style: TextStyle(
+//                                   color: Colors.black,
+//                                   fontSize: 20,
+//                                 )),
+//                           ])),
+//                     ]),
+//               ]),
+//             ),
+//           ),
+//           Container(
+//             margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+//             padding: EdgeInsets.all(0),
+//             height: 70,
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.all(
+//                 Radius.circular(10),
+//               ),
+//               color: Colors.white,
+//               boxShadow: [
+//                 BoxShadow(
+//                   offset: Offset(0.0, 0.4),
+//                   blurRadius: 3,
+//                   color: Colors.grey.shade300,
+//                 )
+//               ],
+//               border: Border.all(color: Colors.grey.shade300),
+//             ),
+//             child: FlatButton(
+//               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+//               onPressed: () {
+// //                   showDialog(
+// //                     context: context,
+// //                     builder: (_) => AddLecture(),
+// //                   );
+//               },
+//               child: Column(children: <Widget>[
+//                 Row(
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     children: <Widget>[
+//                       Padding(
+//                         padding: EdgeInsets.symmetric(horizontal: 5),
+//                         child: Icon(Icons.speaker_notes,
+//                             color: Colors.green, size: 30),
+//                       ),
+//                       Container(
+//                         margin: EdgeInsets.only(left: 2),
+//                         height: 25,
+//                         width: 2,
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.all(
+//                             Radius.circular(10),
+//                           ),
+//                           color: Colors.green,
+//                         ),
+//                       ),
+//                       Container(
+//                         margin: EdgeInsets.symmetric(horizontal: 2),
+//                         height: 45,
+//                         width: 2,
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.all(
+//                             Radius.circular(10),
+//                           ),
+//                           color: Colors.green,
+//                         ),
+//                       ),
+//                       Container(
+//                         margin: EdgeInsets.only(right: 10),
+//                         height: 25,
+//                         width: 2,
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.all(
+//                             Radius.circular(10),
+//                           ),
+//                           color: Colors.green,
+//                         ),
+//                       ),
+//                       Container(
+//                           child: Column(
+//                               mainAxisAlignment: MainAxisAlignment.start,
+//                               crossAxisAlignment: CrossAxisAlignment.start,
+//                               children: <Widget>[
+//                             Text('Announcement',
+//                                 style: TextStyle(
+//                                   color: Colors.black,
+//                                   fontSize: 20,
+//                                 )),
+//                           ])),
+//                     ]),
+//               ]),
+//             ),
+//           ),
+//         ],
+//       ),
+//     )
