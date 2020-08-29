e1a0df36ccf86b47c21fee03f735d02c98349a7d
Haresh Nayak <61956975+hareshnayak@users.noreply.github.com>
2020-08-29 10:49:43 +0530

    Update widgets.dart

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,6 +1,262 @@
 import 'package:flutter/material.dart';
 import 'package:timetable/widgets/functions.dart';
 
+// class AddLecture extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() => AddLectureState
+
+// ();
+// }
+
+// class AddLectureState extends State<AddLecture>
+//     with SingleTickerProviderStateMixin {
+//   AnimationController controller;
+//   Animation<double> scaleAnimation;
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     controller =
+//         AnimationController(vsync: this, duration: Duration
+
+// (milliseconds: 450));
+//     scaleAnimation =
+//         CurvedAnimation(parent: controller, curve: 
+
+// Curves.elasticInOut);
+
+//     controller.addListener(() {
+//       setState(() {});
+//     });
+
+//     controller.forward();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Material(
+//         color: Colors.transparent,
+//         child: ScaleTransition(
+//           scale: scaleAnimation,
+//           child: Container(
+//               margin: EdgeInsets.all(20.0),
+//               padding: EdgeInsets.symmetric(vertical: 10.0, 
+
+// horizontal: 10),
+//               height: 450.0,
+//               decoration: ShapeDecoration(
+//                   color: Colors.white,
+//                   shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(15.0))),
+//               child: Container(
+//                   padding: EdgeInsets.symmetric(horizontal: 10, 
+
+// vertical: 10),
+//                   decoration: BoxDecoration(
+//                       color: Colors.grey.shade200,
+//                           borderRadius: BorderRadius.all
+
+// (Radius.circular(15.0)),
+//                   border: Border.all(width: 5, color: Colors.grey)), 
+
+// // dashed Border
+//                   child: Column(
+//                     children: <Widget>[
+//                       Text('Add Lecture',
+//                           style: TextStyle(
+//                             fontWeight: FontWeight.w600,
+//                             color: Colors.black,
+//                             fontSize: 18,
+//                           )),
+//                        SizedBox(height: 10),
+//                       Container(
+//                         padding: EdgeInsets.symmetric(horizontal: 
+
+// 10),
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.all
+
+// (Radius.circular(10)),
+//                           border: Border.all(color: Colors.grey),
+//                           color: Colors.white,
+//                         ),
+//                         child: TextField(
+//                             decoration: InputDecoration(
+//                                 border: InputBorder.none,
+//                                 hintText: 'meeting link')),
+//                       ),
+//                       SizedBox(height: 20),
+//                       Row(children: <Widget>[
+//                         Text('Start Time',
+//                           style: TextStyle(
+//                             fontWeight: FontWeight.w600,
+//                             color: Colors.black,
+//                             fontSize: 16,
+//                           )),
+//                         Spacer(),
+//                         Text('11:00',
+//                           style: TextStyle(
+//                             fontWeight: FontWeight.w500,
+//                             color: Colors.black,
+//                             fontSize: 16,
+//                           )),
+//                         SizedBox(
+//                           width: 50,
+//                           child:FlatButton(
+//                             padding: EdgeInsets.all(0),
+//                         onPressed:(){},
+//                           child: Icon(Icons.timer, color: Colors.blue)
+//                         ),),
+//                       ]),
+//                       Row(children: <Widget>[
+//                         Text('End Time',
+//                           style: TextStyle(
+//                             fontWeight: FontWeight.w600,
+//                             color: Colors.black,
+//                             fontSize: 16,
+//                           )),
+//                         Spacer(),
+//                         Text('12:00',
+//                           style: TextStyle(
+//                             fontWeight: FontWeight.w500,
+//                             color: Colors.black,
+//                             fontSize: 16,
+//                           )),
+//                         SizedBox(
+//                           width: 50,
+//                           child:FlatButton(
+//                             padding: EdgeInsets.all(0),
+//                         onPressed:(){},
+//                           child: Icon(Icons.timer, color: Colors.blue)
+//                         ),),
+//                       ]), 
+//                       SizedBox(height: 20),                      
+//                      Container(
+//                         padding: EdgeInsets.symmetric(horizontal: 
+
+// 10),
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.all
+
+// (Radius.circular(10)),
+//                           border: Border.all(color: Colors.grey),
+//                           color: Colors.white,
+//                         ),
+//                         child: TextField(
+//                           maxLines: 5,
+//                             decoration: InputDecoration(
+//                                 border: InputBorder.none,
+//                                 hintText: 'Additional Information')),
+//                       ),
+//                        Container(
+//                                   margin: EdgeInsets.all(10),
+//                                   height: 40,
+//                                   width: MediaQuery.of
+
+// (context).size.width,
+//                                   child: FloatingActionButton.extended(
+//                                 onPressed:(){},
+//                                 label: Text('Add Lecture'),
+//                                   icon: Icon(Icons.check_circle_outline),
+//                                     elevation: 1,
+//                                 ),),
+//                     ],
+//                   ))),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+Widget SubjectCR(){
+return Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              padding: EdgeInsets.all(0),
+              height: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0.0, 0.4),
+                    blurRadius: 3,
+                    color: Colors.grey.shade300,
+                  )
+                ],
+                border: Border.all(color: Colors.grey.shade300),
+              ),
+              child: FlatButton(
+                padding: EdgeInsets.symmetric(horizontal: 10, 
+
+vertical: 10),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) => AddLecture(),
+                  );
+                },
+                child: Column(children: <Widget>[
+                  Row(
+                      crossAxisAlignment: 
+
+CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 
+
+2),
+                          height: 25,
+                          width: 2,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            color: Colors.green,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 10),
+                          height: 45,
+                          width: 2,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            color: Colors.green,
+                          ),
+                        ),
+                        Container(
+                            child: Column(
+                                mainAxisAlignment: 
+
+MainAxisAlignment.start,
+                                crossAxisAlignment: 
+
+CrossAxisAlignment.start,
+                                children: <Widget>[
+                              Text('Digital Design',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                  )),
+                              SizedBox(height: 2),
+                              Text('by Prof. Rajesh Rohilla',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  )),
+                            ])),
+                      ]),
+                ]),
+              ),
+            )
+}
+
+
 Widget buildWaitingScreen() {
   return Scaffold(
     backgroundColor: Colors.white,
