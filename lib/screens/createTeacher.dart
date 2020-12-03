603ded27678f0f8a5e9d51131db53c18d1ad6a7c
Haresh Nayak <61956975+hareshnayak@users.noreply.github.com>
2020-12-04 00:08:38 +0530

    Update createTeacher.dart

--- lib/screens/createTeacher.dart
+++ lib/screens/createTeacher.dart
@@ -8,6 +8,39 @@ class CreateTeacher extends StatefulWidget {
 class _CreateTeacherState extends State<CreateTeacher> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(      
+      body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: ListView(            
+            children: <Widget>[
+              SizedBox(height: 10),
+              headingText('Teacher'),         
+              showInput('Name'), 
+              showInput('College'), 
+              showInput('Email'),
+              showPasswordInput(),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.pinkAccent),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: FlatButton(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),             
+            ],
+          ),
+      ),
+    );
   }
 }
