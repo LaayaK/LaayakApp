b117d89b211753f4828d5055e2b005e993389ad4
Haresh Nayak <61956975+hareshnayak@users.noreply.github.com>
2020-12-03 23:10:51 +0530

    Update createStudent.dart

--- lib/screens/createStudent.dart
+++ lib/screens/createStudent.dart
@@ -16,6 +16,104 @@ class _CreateStudentState extends State<CreateStudent> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(      
+      body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 10),
+              headingText('Student'),
+              showInput('Class Code'), 
+              showInput('Name'), 
+              showInput('Roll No.'), 
+              showInput('Email'),               
+              showPasswordInput(),                            
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.pinkAccent),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: FlatButton(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),             
+            ],
+          ),
+      ),
+    );
   }
 }
+
+ Widget showInput(String hint) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: new TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: new InputDecoration(
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          errorBorder: InputBorder.none,
+          disabledBorder: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          hintText: hint,
+          hintStyle: TextStyle(
+            fontSize: 15,
+            color: Colors.grey
+          ),
+        ),
+//         validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+//         onSaved: (value) => _email = value.trim(),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(14),
+        color: Color(0xFFE5E5E5),
+      ),
+    );
+  }
+Widget showPasswordInput() {
+  return Container(
+    margin: EdgeInsets.all(10),
+    child: new TextFormField(
+      maxLines: 1,
+      obscureText: true,
+      autofocus: false,
+      onChanged: (value) {
+//           setState(() {
+//             _isButtonDisabled = false;
+//             print('password tapped');
+//           });
+      },
+      decoration: new InputDecoration(
+        border: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        errorBorder: InputBorder.none,
+        disabledBorder: InputBorder.none,
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        hintText: 'Enter Password',
+        hintStyle: TextStyle(fontSize: 15, color: Colors.grey),
+      ),
+//         validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+//         onSaved: (value) => _password = value.trim(),
+    ),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(14),
+      color: Color(0xFFE5E5E5),
+    ),
+  );
+}
+// }
