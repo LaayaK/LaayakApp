0b8d7392a3ac1a9d86ad79141d122490f00e584b
Haresh Nayak <61956975+hareshnayak@users.noreply.github.com>
2020-12-04 09:34:25 +0530

    Update homePage.dart

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -24,7 +24,94 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Column(
+          child: 
+         Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: ListView(          
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.network(
+                        'https://usersthink.com/img/99-free-images-small/02-usersthink-stock-image-small.jpg',
+                        fit: BoxFit.fitWidth),
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  Positioned(
+                    top: 50,
+                    left: (MediaQuery.of(context).size.width - 100) / 10,
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: Image.network('https://laayak2.web.app/logo.png'),
+                    ),
+                  ),
+                  Positioned(
+                    top: 95,
+                    left: (MediaQuery.of(context).size.width - 170),
+                    child: headingText('LaayaK'),
+                  ),
+                ],
+              ),
+            ),        
+            headingText3('Welcome'),
+            headingText3(
+                'You haven\'t been added to any class yet. \n\n We\'ll update you soon when your are'),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.blue),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: FlatButton(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Go to Home Page',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.pinkAccent),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: FlatButton(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Log out',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ), 
+          Column(
             children: [
               Text('TEACHER PAGE!!! Find classes!'),
               FlatButton(child: Text('Go to Home Page'),
